4186e0b85c5f111d5068e7983de8610fd1cb5eda
Shenal Menuka <Shenal Menuka@DESKTOP-RGO8796>
2019-04-15 21:36:59 +0530

    added header and home layouts with images on root

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -34,6 +34,17 @@ namespace WebSite.Controllers
             return View();
         }
 
+        public IActionResult Header()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
