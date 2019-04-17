4110fabd5068233e03d28bc42e3f732ebcf2a549
Shenal Menuka <Shenal Menuka@DESKTOP-RGO8796>
2019-04-17 23:02:37 +0530

    made changes

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -49,6 +49,10 @@ namespace WebSite.Controllers
             return View();
         }
 
+        public IActionResult PostAd()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
